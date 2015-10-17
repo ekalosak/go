873e44dbbc0b7b7add58a2b16b361bf325cf5d13
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2015-10-17 15:32:53 -0400

    trying to make placing stones in surrounded positions invalid, but it's hard

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import itertools as itt
+from copy import copy
 
 import numpy as np
 import pandas as pd
@@ -15,6 +16,7 @@ LINE_COLOR = 0, 0, 0
 LINE_WIDTH = 2
 SELECTOR_COLOR = 255, 0, 0
 SELECTOR_SIZE = LINE_WIDTH + 2
+N_SURROUNDED = 4
 
 class Player(object):
 
@@ -30,7 +32,7 @@ class Player(object):
 class Stone(object):
 
     def __init__(self, y, x, player, grid):
-        self.location = y, x
+        self.location = self.y, self.x = y, x
         self.player = player
         self.size = STONE_SIZE
         self.grid = grid
@@ -38,8 +40,31 @@ class Stone(object):
     def draw(self, screen):
         pg.draw.circle(screen,
                 self.player.color,
-                self.grid.get_point(*self.location),
+                self.grid.get_point(*self.location[::-1]),
                 self.size)
+
+    def surrounding_locations(self):
+        locs = []
+
+        for dx in [-1, 1]:
+            x = self.x + dx
+            y = self.y
+            if x >= 0 and\
+                    x < self.grid.dims[1] and\
+                    y >= 0 and\
+                    y < self.grid.dims[0]:
+                locs.append((y, x))
+
+        for dy in [-1, 1]:
+            x = self.x
+            y = self.y + dy
+            if x >= 0 and\
+                    x < self.grid.dims[1] and\
+                    y >= 0 and\
+                    y < self.grid.dims[0]:
+                locs.append((y, x))
+
+        return locs
 
 class Grid(object):
 
@@ -84,29 +109,49 @@ class Selector(object):
     def __init__(self, color, size, grid):
         self.color = color
         self.size = size
-        self.location = [0, 0]
+        self.location = self.y, self.x = [0, 0]
         self.grid = grid
 
     def move(self, dirn):
 
-        if dirn == pg.K_LEFT:
-            if self.location[0] - 1 >= 0:
-                self.location[0] = self.location[0] - 1
-        elif dirn == pg.K_RIGHT:
-            if self.location[0] + 1 < self.grid.dims[0]:
-                self.location[0] = self.location[0] + 1
-        elif dirn == pg.K_UP:
-            if self.location[1] - 1 >= 0:
-                self.location[1] = self.location[1] - 1
+        if dirn == pg.K_UP:
+            if self.y - 1 >= 0:
+                self.y = self.y - 1
         elif dirn == pg.K_DOWN:
-            if self.location[1] + 1 < self.grid.dims[1]:
-                self.location[1] = self.location[1] + 1
+            if self.y + 1 < self.grid.dims[0]:
+                self.y = self.y + 1
+        elif dirn == pg.K_LEFT:
+            if self.x - 1 >= 0:
+                self.x = self.x - 1
+        elif dirn == pg.K_RIGHT:
+            if self.x + 1 < self.grid.dims[1]:
+                self.x = self.x + 1
+
+        self.location = self.y, self.x
 
     def draw(self, screen):
         pg.draw.circle(screen,
                 SELECTOR_COLOR,
-                self.grid.get_point(*self.location),
+                self.grid.get_point(*self.location[::-1]),
                 SELECTOR_SIZE)
+
+class State(object):
+
+    def __init__(self, dims, turn, board_stones, bowl_stones):
+        self.dims = dims
+        self.turn = turn
+        self.board_stones = board_stones
+        self.bowl_stones = bowl_stones
+
+    def stone_at(self, loc):
+        for stone in self.board_stones:
+            if stone.location == loc:
+                return True
+        return False
+
+    def draw(self, screen):
+        for stone in self.board_stones:
+            stone.draw(screen)
 
 class Board(object):
 
@@ -115,17 +160,24 @@ class Board(object):
         self.players = Player(WHITE), Player(BLACK)
         self.grid = Grid(screensize, x, y)
         self.dims = self.grid.dims
-        self.states = []
-        self.state = np.empty(shape = self.dims,
-                dtype = Stone)
+        self.states = [State(self.dims, 0, [], [])]
         self.whose_turn = self.players[0]
         self.selector = Selector(color = SELECTOR_COLOR,
                 size = SELECTOR_SIZE,
                 grid = self.grid)
-        self.stones = []
+
+    def set_state(self, state):
+        self.states.append(state)
+
+    def get_state(self):
+        return self.states[-1]
 
     def move_select(self, dirn):
         self.selector.move(dirn)
+
+    def stone_at(self, loc):
+        state = self.get_state()
+        return state.stone_at(loc)
 
     def place_stone(self):
         loc = self.selector.location
@@ -133,34 +185,57 @@ class Board(object):
                 player = self.whose_turn,
                 grid = self.grid)
         if self.valid_move(new_stone):
-            self.stones.append(new_stone)
-            self.next_turn()
+            self.next_turn(new_stone)
 
     def valid_move(self, new_stone):
-        # Most of the game logic is here
-        for stone in self.stones:
-            if stone.location == new_stone.location:
-                return False
+        # avoid collisions
+        if self.stone_at(new_stone.location):
+            return False
+
+        # no placing in a surrounded position
+        # FIXME
+        nsurrounding = 0
+        for loc in new_stone.surrounding_locations():
+            if self.stone_at(loc):
+                nsurrounding = nsurrounding + 1
+        if nsurrounding == N_SURROUNDED:
+            return False
+
+        # TODO # no placing into a previous state
+
+        # otherwise valid move
         return True
 
-    def next_turn(self):
-        if self.whose_turn == self.players[0]:
-            self.whose_turn = self.players[1]
-        elif self.whose_turn == self.players[1]:
-            self.whose_turn = self.players[0]
+    def next_turn(self, new_stone):
+        # make a new state
+        old_state = self.get_state()
+        new_state = State(self.dims,
+                old_state.turn + 1,
+                old_state.board_stones + [new_stone],
+                old_state.bowl_stones)
+
+        # move captured stones into the bin
+        captured_stones = self.capture_stones(new_state, new_stone)
+        for cstone in captured_stones:
+            for bstone in new_state.board_stones:
+                if cstone == bstone:
+                    new_state.board_stones.remove(bstone)
+                    new_state.bowl_stones.append(cstone)
+
+        # switch player
+        self.whose_turn = self.players[new_state.turn % len(self.players)]
+        # set the new state to the current state
+        self.set_state(new_state)
+
+    def capture_stones(self, state, stone):
+        # TODO
+        return []
 
     def draw(self, screen):
         self.grid.draw(screen)
-        for stone in self.stones:
-            stone.draw(screen)
+        state = self.get_state()
+        state.draw(screen)
         self.selector.draw(screen)
-
-    def validate_selector(self):
-        pass
-    def validate_state(self, state):
-        # check that the move was valid
-        #   remove captured stones
-        pass
 
 def test():
     print "Passed tests!!"
