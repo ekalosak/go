4d1c8c3bedf8fcc61c631ee59307b4587c744c01
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-05-23 13:16:09 -0700

    updated testing format

--- tests.py
+++ tests.py
@@ -6,21 +6,27 @@ from play_in_terminal import endgame
 # Import constants used
 from play_in_terminal import PASS
 
-def test_endgame():
+class TestEndgame:
 
     # Moves is list of (int, Move)
     #   where int indicates the player, a base.int
     #   and Move indicates the move made, either PASS or [int, int, ..]
     #       denoting the position of the stone placed
 
-    # First, trivial case
-    moves = [(1, PASS)]
-    assert(endgame(moves, 1)) # a pass with one player ends the game
+    def test_trivial_endgame(self):
+        # First, trivial case
+        moves = [(1, PASS)]
+        assert(endgame(moves, 1)) # a pass with one player ends the game
 
-    # Another trivial case
-    moves = [(1, PASS), (2, PASS)]
-    assert(endgame(moves, 2)) # a pass with one player ends the game
+    def test_trivial_endgame2(self):
+        # Another trivial case
+        moves = [(1, PASS), (2, PASS)]
+        assert(endgame(moves, 2)) # a pass with one player ends the game
 
-    # A final case with some play before it
-    moves = [(1, [1,1]), (2, [1,2]), (1, PASS), (2, PASS)]
-    assert(endgame(moves, 2)) # a pass with one player ends the game
+    def test_regular_endgame(self):
+        # A final case with some play before it
+        moves = [(1, [1,1]), (2, [1,2]), (1, PASS), (2, PASS)]
+        assert(endgame(moves, 2)) # a pass with one player ends the game
+
+def test_valid_move():
+    pass
