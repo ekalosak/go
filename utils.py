390cc609139da6ef8f2770bae34bf1b3402851fc
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-05-23 12:23:02 -0700

    reduced utils

--- utils.py
+++ utils.py
@@ -1,11 +1,4 @@
 import logging
-import time
-import pdb
-import datetime
-import numpy
-import re
-
-import geopy
 
 def make_logger(name, verbose = True):
     # https://docs.python.org/2/howto/logging-cookbook.html
@@ -49,37 +42,3 @@ def loggable(log, debuglevel="debug"):
             return result
         return inner
     return wrap
-
-def epoch(datestring):
-    # "d/m/y h:m:s" -> int
-    #   time since 1/1/1970
-    date = datestring.split(' ')[0]
-    tim = datestring.split(' ')[1]
-    day = date.split('/')[1]
-    month = date.split('/')[0]
-    year = date.split('/')[2]
-    hour = tim.split(':')[0]
-    timedata = day, month, year, hour
-    timedata = map(lambda x: int(x), timedata)
-    day, month, year, hour = timedata
-    epo = (datetime.datetime(year,month,day,hour) -
-            datetime.datetime(1970,1,1)).total_seconds()
-    return epo
-
-def latlon(addr, log):
-    geolocator = geopy.geocoders.Nominatim()
-    log.debug('searching for <{}> using <{}>'.format(addr, geolocator))
-    location = geolocator.geocode(addr)
-    log.debug('found <{}>'.format(location))
-    return location.latitude, location.longitude
-
-def approx_eq(f0, f1, prec = 4):
-    assert type(f0) == float
-    assert type(f1) == float
-    n0, n1 = map(lambda f: round(f, prec), [f0, f1])
-    return n0 == n1
-
-def string_to_type(typestring):
-    # "<type 'numpy.int64'>" -> <type 'numpy.int64'>
-    tps = re.match('(.*)\'(.*)\'(.*)', qq).groups()[1]
-    tp = eval(tps)
